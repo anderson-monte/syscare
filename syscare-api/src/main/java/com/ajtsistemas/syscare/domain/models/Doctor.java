4464fb554c168476bd3ee56efe7719445cfc2dae
Anderson Monte <andersonsmont@gmail.com>
2023-08-18 12:27:02 -0300

    Some changes in Doctor e HealthCenter

--- syscare-api/src/main/java/com/ajtsistemas/syscare/domain/models/Doctor.java
+++ syscare-api/src/main/java/com/ajtsistemas/syscare/domain/models/Doctor.java
@@ -1,15 +1,18 @@
 package com.ajtsistemas.syscare.domain.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.List;
 
 @Entity
+@Getter
 public class Doctor {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
     private String specialty;
+    @OneToMany
+    private List<HealthCenter> healthCenter;
 }
--- syscare-api/src/main/java/com/ajtsistemas/syscare/domain/models/HealthCenter.java
+++ syscare-api/src/main/java/com/ajtsistemas/syscare/domain/models/HealthCenter.java
@@ -4,12 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class HealthCenter {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
-
 }
